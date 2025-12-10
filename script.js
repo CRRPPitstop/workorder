9b43e2fa54c85adbdea1f5ea69cc568ab29611a6
CRRPPitstop <nedevelopment2022@gmail.com>
2025-12-09 19:29:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,6 @@ document.getElementById("serviceForm").addEventListener("submit", async e => {
                     { name: "Time", value: time.value },
                     { name: "Vehicle", value: vehicle.value },
                     { name: "Plate #", value: plate.value },
-                    { name: "Employee Name", value: employee.value },
                     { name: "Job Type", value: jobType.value },
                     { name: "Work Performed", value: workPerformed.value },
                     {
@@ -138,5 +137,6 @@ function showMessage(msg, type) {
     box.textContent = msg;
     box.className = `message ${type}`;
 }
+
 
 
